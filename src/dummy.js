d402775a643d51b23e0b57e47b472d595b6244d8
tarlepp <tarmo.leppanen@kase.fi>
2015-08-29 21:30:10 +0300

    Initial commit.

---
+++ src/dummy.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Dummy JS file for smart IDEs like php/webStorm.
+ *
+ * Purpose of this file is to help IDE to use autocomplete features.
+ */
+
+var layout = {
+  menuItem: {
+    state: string,
+    title: string,
+    access: number
+  }
+};
+
+var settings = {
+  backendUrl: string,
+  frontend: {
+    ports: {
+      production: number,
+      development: number
+    }
+  }
+};
